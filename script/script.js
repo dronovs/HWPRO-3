08431ea4f31aeba67192130a3dd21e1721379df4
dronovs <od270891dss@gmail.com>
2021-09-01 16:29:11 +0300

    update

--- script/script.js
+++ script/script.js
@@ -1,87 +1,63 @@
-let operator = prompt('choose you operation: +, -, /, *, cos, sin or pow', '+');
-
 let firstOperand = 0;
 let secondOperand = 0;
 let result = 0;
+let confirming = false;
 
-if (operator === '+') {
+do {
+    let operator = prompt('choose your operation: +, -, /, *, cos, sin or pow', '+');
     firstOperand = +prompt('please enter first operand');
-    secondOperand = +prompt('please enter second operand');
     if (isNaN(firstOperand)) {
         while (isNaN(firstOperand)) {
-            firstOperand = +prompt('enter first NUMBER please');
+            firstOperand = +prompt('enter NUMBER please');
         }
     }
-    if (isNaN(secondOperand)) {
-        while (isNaN(secondOperand)) {
-            secondOperand = +prompt('enter second NUMBER please');
+
+    if (operator === '+') {
+        secondOperand = +prompt('please enter second operand');
+        if (isNaN(secondOperand)) {
+            while (isNaN(secondOperand)) {
+                secondOperand = +prompt('enter NUMBER please');
+            }
         }
+        result = `sum: ${firstOperand} + ${secondOperand} = ${firstOperand + secondOperand}`;
+    } else if (operator === '-') {
+        secondOperand = +prompt('please enter second operand');
+        if (isNaN(secondOperand)) {
+            while (isNaN(secondOperand)) {
+                secondOperand = +prompt('enter NUMBER please');
+            }
+        }
+        result = `diff: ${firstOperand} - ${secondOperand} = ${firstOperand - secondOperand}`;
+    } else if (operator === '/') {
+        secondOperand = +prompt('please enter second operand');
+        if (isNaN(secondOperand)) {
+            while (isNaN(secondOperand)) {
+                secondOperand = +prompt('enter NUMBER please');
+            }
+        }
+        result = `div: ${firstOperand} / ${secondOperand} = ${firstOperand / secondOperand}`;
+    } else if (operator === '*') {
+        secondOperand = +prompt('please enter second operand');
+        if (isNaN(secondOperand)) {
+            while (isNaN(secondOperand)) {
+                secondOperand = +prompt('enter NUMBER please');
+            }
+        }
+        result = `mult: ${firstOperand} * ${secondOperand} = ${firstOperand * secondOperand}`;
+    } else if (operator === 'pow') {
+        secondOperand = +prompt('please enter second operand');
+        if (isNaN(secondOperand)) {
+            while (isNaN(secondOperand)) {
+                secondOperand = +prompt('enter NUMBER please');
+            }
+        }
+        result = `pow: ${firstOperand} ** ${secondOperand} = ${Math.pow(firstOperand, secondOperand)}`;
+    } else if (operator === 'sin') {
+        result = `sin: ${firstOperand} = ${Math.sin(firstOperand)}`;
+    } else if (operator === 'cos') {
+        result = `cos: ${firstOperand} = ${Math.cos(firstOperand)}`;
     }
-    result = `sum: ${firstOperand} + ${secondOperand} = ${firstOperand + secondOperand}`;
     console.log(result);
     alert(result);
-} else if (operator === '-') {
-    firstOperand = +prompt('please enter first operand');
-    secondOperand = +prompt('please enter second operand');
-    if (isNaN(firstOperand)) {
-        while (isNaN(firstOperand)) {
-            firstOperand = +prompt('enter first NUMBER please');
-        }
-    }
-    if (isNaN(secondOperand)) {
-        while (isNaN(secondOperand)) {
-            secondOperand = +prompt('enter second NUMBER please');
-        }
-    }
-    result = `diff: ${firstOperand} - ${secondOperand} = ${firstOperand - secondOperand}`;
-    console.log(result);
-    alert(result);
-} else if (operator === '/') {
-    firstOperand = +prompt('please enter first operand');
-    secondOperand = +prompt('please enter second operand');
-    if (isNaN(firstOperand)) {
-        while (isNaN(firstOperand)) {
-            firstOperand = +prompt('enter first NUMBER please');
-        }
-    }
-    if (isNaN(secondOperand)) {
-        while (isNaN(secondOperand)) {
-            secondOperand = +prompt('enter second NUMBER please');
-        }
-    }
-    result = `div: ${firstOperand} / ${secondOperand} = ${firstOperand / secondOperand}`;
-    console.log(result);
-    alert(result);
-} else if (operator === '*') {
-    firstOperand = +prompt('please enter first operand');
-    secondOperand = +prompt('please enter second operand');
-    if (isNaN(firstOperand)) {
-        while (isNaN(firstOperand)) {
-            firstOperand = +prompt('enter first NUMBER please');
-        }
-    }
-    if (isNaN(secondOperand)) {
-        while (isNaN(secondOperand)) {
-            secondOperand = +prompt('enter second NUMBER please');
-        }
-    }
-    result = `div: ${firstOperand} * ${secondOperand} = ${firstOperand * secondOperand}`;
-    console.log(result);
-    alert(result);
-} else if (operator === 'pow') {
-    firstOperand = +prompt('please enter first operand');
-    secondOperand = +prompt('please enter second operand');
-    if (isNaN(firstOperand)) {
-        while (isNaN(firstOperand)) {
-            firstOperand = +prompt('enter first NUMBER please');
-        }
-    }
-    if (isNaN(secondOperand)) {
-        while (isNaN(secondOperand)) {
-            secondOperand = +prompt('enter second NUMBER please');
-        }
-    }
-    result = `pow: ${firstOperand} ** ${secondOperand} = ${Math.pow(firstOperand, secondOperand)}`;
-    console.log(result);
-    alert(result);
-}
+    confirming = confirm('another one calculation?');
+} while (confirming === true);
